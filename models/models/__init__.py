d5bdef26294affbf071be3488f992e7d12e78b4f
Don Michael Feeney Jr <158860247+dfeen87@users.noreply.github.com>
2025-12-11 06:10:31 -0500

    Create __init__.py

---
+++ models/models/__init__.py
@@ -0,0 +1,14 @@
+from .candidate import Candidate, SoulboundToken, SBTType
+from .market import GSTIMetrics, MarketRegime, MarketUpdate
+from .query import HiringQuery, QueryResult
+
+__all__ = [
+    "Candidate",
+    "SoulboundToken",
+    "SBTType",
+    "GSTIMetrics",
+    "MarketRegime",
+    "MarketUpdate",
+    "HiringQuery",
+    "QueryResult"
+]
